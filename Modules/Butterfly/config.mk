70a57788544bf2e73f503d192c444526746c50e2
anonymous4penguin <anonymous4penguin@gmail.com>
2026-05-08 18:47:01 +0530

    Add files via upload

---
+++ Modules/Butterfly/config.mk
@@ -0,0 +1,23 @@
+export DESIGN_NAME = top
+
+export PLATFORM = nangate45
+
+export VERILOG_FILES = ./designs/src/generated/butterfly.v
+
+export CLOCK_PORT = clk
+
+export CLOCK_PERIOD = 10
+
+export SDC_FILE = ./designs/src/butterfly/constraint.sdc
+
+# ---------------------------------------------------
+# FLOORPLAN SETTINGS
+# ---------------------------------------------------
+
+export CORE_UTILIZATION = 40
+
+export PLACE_DENSITY = 0.50
+
+export CORE_ASPECT_RATIO = 1
+
+export CORE_MARGIN = 2
