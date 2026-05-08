21a2892d76700f7f838f0e01221b80eed6ffe605
anonymous4penguin <anonymous4penguin@gmail.com>
2026-05-08 18:48:54 +0530

    Add files via upload

---
+++ Modules/Rotator/config.mk
@@ -0,0 +1,23 @@
+export DESIGN_NAME = top
+
+export PLATFORM = nangate45
+
+export VERILOG_FILES = ./designs/src/generated/rotator.v
+
+export CLOCK_PORT = clk
+
+export CLOCK_PERIOD = 10
+
+export SDC_FILE = ./designs/src/rotator/constraint.sdc
+
+# ---------------------------------------------------
+# FLOORPLAN SETTINGS
+# ---------------------------------------------------
+
+export CORE_UTILIZATION = 40
+
+export PLACE_DENSITY = 0.50
+
+export CORE_ASPECT_RATIO = 1
+
+export CORE_MARGIN = 2
